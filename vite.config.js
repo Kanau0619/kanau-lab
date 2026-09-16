4cc178be6b2c840d3ba978e0197c001da62ec87a
Kanau <manweihu@gmail.com>
2026-09-16 19:49:55 +0800

    Support branch preview base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "github" ? "/kanau-lab/" : "/",
-  plugins: [react()],
-}));
+export default defineConfig(({ mode }) => {
+  let base = "/";
+
+  if (mode === "github") {
+    base = "/kanau-lab/";
+  } else if (mode === "github-preview") {
+    base = "/kanau-lab/preview/";
+  }
+
+  return {
+    base,
+    plugins: [react()],
+  };
+});
