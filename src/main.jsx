2b18c0213b6070b676466c30113f120a48f71488
Kanau <manweihu@gmail.com>
2026-09-16 16:46:32 +0800

    Load portfolio polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
+import "./portfolio-polish.css";
 
 const routerBase = import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
 
